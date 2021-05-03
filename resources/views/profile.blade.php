8c4f775d51d33f87a5bfed9f3e7e2fa9b626f3e6
islam <islaaamraslan@gmail.com>
2021-05-03 18:22:00 +0300

    the new virstion

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../library/fontawesome-free-5.15.2-web/css/all.min.css">
+    <link rel="stylesheet" href="/css/profile.css">
+</head>
+<body>
+
+      <!-- profile -->
+    <div class="row py-5 px-4 col-md-12">
+        <div class="col-md-12 mx-auto">
+            <!-- Profile widget -->
+            <div class="bg-white shadow rounded overflow-hidden big">
+              <div class="px-4 pt-0 pb-4 cover">
+                <div class="media align-items-end profile-head">
+                    <div class="profile mr-3">
+                      <img class="profile-pic rounded mb-2 img-thumbnail" src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="..." >
+                     <div class="p-image media-body mb-5 text-white">
+                        <i class="upload-button"><img src="https://img.icons8.com/android/35/000000/camera.png"/></i>
+                        <input class="file-upload" type="file" accept="image/*"/>
+                    </div>
+                  </div>
+                    <div class="media-body mb-5 ">
+                      <h4 class="mt-0 mb-5 ml-5 h4 par text-white" >Mark Williams</h4>
+                  </div>   
+                   
+                </div>
+            </div>
+                         
+
+                   
+                <div class="bg-light p-4 d-flex justify-content-end text-center">
+                    <ul class="list-inline mb-0 mr-5">
+
+                        <li class="list-inline-item" style="margin-left: 20px;">
+                            <h5 class="font-weight-bold mb-0 d-block"><img src="https://img.icons8.com/fluent/48/000000/card-wallet.png"/></h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Awaiting payment</small>
+                        </li>
+                        <li class="list-inline-item" style="margin-left: 20px;">
+                            <h5 class="font-weight-bold mb-0 d-block"><img src="https://img.icons8.com/cotton/48/000000/shipping-center.png"/></h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>For shipping</small>
+                        </li>
+                        <li class="list-inline-item" style="margin-left: 20px;">
+                            <h5 class="font-weight-bold mb-0 d-block"><img src="https://img.icons8.com/color/48/000000/free-shipping.png"/></h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>loaded </small>
+                        </li>
+                        
+                    </ul>
+                </div>
+                </div> 
+                <!-- profile info -->
+                <div class="px-4 py-3">
+                  
+                  <div class="row name">
+                    <div class="col-sm-1">
+                      <h6 class="mb-0">Full Name</h6> 
+                    </div>
+                    <div class="col input1 ">
+                        <p>Mark Williams</p>
+                  </div>
+                  </div>
+                  <div class="col-md-3 mt-3 mr-3"> 
+                    <a href="#" class="btn edit btn-sm  btn-success">Edit Name</a>
+                  <a href="#" class="btn save btn-sm  btn-success">Save</a>
+              </div> 
+                    <h5 class="mt-5">About</h5>
+                    <div class="p-1 rounded shadow-sm bg-light">
+                     
+                       
+                          <div class="row">
+                            <div class="col-sm-1">
+                              <h6 class="mb-0">Location</h6> 
+                            </div>
+                            <div class="col input">
+                                <p>tulkarm</p>
+                          </div>
+                          </div>  
+                          <div class="row">
+                            <div class="col-sm-1">
+                              <h6 class="mb-0">Email</h6> 
+                            </div>
+                            <div class="col input">
+                                <p>Mark Williams@gmail.com</p>
+                          </div>
+                          </div>    
+                          <div class="row">
+                            <div class="col-sm-1">
+                              <h6 class="mb-0">Password</h6> 
+                            </div>
+                            <div class="col input">
+                                <p>Mark Williams@gmail.com</p>
+                          </div>
+                          </div>                  
+                    </div>
+                    <div class="col-md-3 mt-1 mr-3"> 
+                      <a href="#" class="btn edit2  btn-sm  btn-success">Edit profile</a>
+                    <a href="#" class="btn save2  btn-sm btn-success">Save</a>
+                </div> 
+                </div>
+              </div>
+
+            </div>
+  
+    
+
+    <!-- navbar -->
+    <div class="clear"></div>
+    <nav class="navbar fixed-bottom navbar-light bg-light" >
+        <a class="navbar-brand" id="name" href="#"> <img src="../img/online-shopping.png" style="width:45px; height: 45px;"> Online Shopping</a>
+  <button class="navbar-toggler" id="btn" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" style="border: none; outline: none;">
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+    width="55" height="55"
+    viewBox="0 0 172 172"
+    style=" fill:#000000;"><defs><linearGradient x1="65.17367" y1="21.50358" x2="105.51483" y2="146.43292" gradientUnits="userSpaceOnUse" id="color-1_yFAVSbDZOJll_gr1"><stop offset="0" stop-color="#34495e"></stop><stop offset="0.587" stop-color="#003456"></stop><stop offset="1" stop-color="#2e53cc"></stop></linearGradient></defs><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g><path d="M143.33333,21.5h-114.66667c-3.95958,0 -7.16667,3.20708 -7.16667,7.16667v114.66667c0,3.95958 3.20708,7.16667 7.16667,7.16667h114.66667c3.95958,0 7.16667,-3.20708 7.16667,-7.16667v-114.66667c0,-3.95958 -3.20708,-7.16667 -7.16667,-7.16667z" fill="url(#color-1_yFAVSbDZOJll_gr1)"></path><path d="M121.83333,75.25h-25.08333v-25.08333c0,-3.95958 -3.20708,-7.16667 -7.16667,-7.16667h-7.16667c-3.95958,0 -7.16667,3.20708 -7.16667,7.16667v25.08333h-25.08333c-3.95958,0 -7.16667,3.20708 -7.16667,7.16667v7.16667c0,3.95958 3.20708,7.16667 7.16667,7.16667h25.08333v25.08333c0,3.95958 3.20708,7.16667 7.16667,7.16667h7.16667c3.95958,0 7.16667,-3.20708 7.16667,-7.16667v-25.08333h25.08333c3.95958,0 7.16667,-3.20708 7.16667,-7.16667v-7.16667c0,-3.95958 -3.20708,-7.16667 -7.16667,-7.16667z" fill="#000000" opacity="0.05"></path><path d="M121.83333,77.04167h-26.875v-26.875c0,-2.967 -2.408,-5.375 -5.375,-5.375h-7.16667c-2.967,0 -5.375,2.408 -5.375,5.375v26.875h-26.875c-2.967,0 -5.375,2.408 -5.375,5.375v7.16667c0,2.967 2.408,5.375 5.375,5.375h26.875v26.875c0,2.967 2.408,5.375 5.375,5.375h7.16667c2.967,0 5.375,-2.408 5.375,-5.375v-26.875h26.875c2.967,0 5.375,-2.408 5.375,-5.375v-7.16667c0,-2.967 -2.408,-5.375 -5.375,-5.375z" fill="#000000" opacity="0.07"></path><path d="M82.41667,46.58333h7.16667c1.978,0 3.58333,1.60533 3.58333,3.58333v71.66667c0,1.978 -1.60533,3.58333 -3.58333,3.58333h-7.16667c-1.978,0 -3.58333,-1.60533 -3.58333,-3.58333v-71.66667c0,-1.978 1.60533,-3.58333 3.58333,-3.58333z" fill="#ffffff"></path><path d="M125.41667,82.41667v7.16667c0,1.978 -1.60533,3.58333 -3.58333,3.58333h-71.66667c-1.978,0 -3.58333,-1.60533 -3.58333,-3.58333v-7.16667c0,-1.978 1.60533,-3.58333 3.58333,-3.58333h71.66667c1.978,0 3.58333,1.60533 3.58333,3.58333z" fill="#ffffff"></path></g></g></svg>  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="cart.html"><img src="https://img.icons8.com/material-two-tone/30/000000/fast-cart.png"/> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=" HomePage.html"><img src="https://img.icons8.com/plumpy/30/000000/home.png"/></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><img src="https://img.icons8.com/plumpy/30/000000/chat.png"/></a>
+      </li>
+    </ul>
+ 
+  </div>
+      </nav>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="/js/profile.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Customercontroler;
+use App\Http\Controllers\admin;
+use App\Http\Controllers\adminDBcontroller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+   }); 
+
+Route::view('signup','logInPage');
+Route::post('signup', [Customercontroler::class, 'AddCustomer']);
+//Route::view('islam', 'islam');
+
+Route::view('home','home');
+Route::view('customer_login','CustomerLogIn');
+
+//admin route
+Route::post('admin/login',[admin::class,'login']);
+Route::view('admin/login','adminLogeIn');
+
+
+//sgin route
+Route::post('customer_login',[Customercontroler::class,'coustomerLogin']);
+
+
+//dashBord route
+Route::view('adminDashbord','adminDashBord');
+Route::get('adminDashbord',[adminDBcontroller::class,'show']);
+Route::post('adminDashbord',[adminDBcontroller::class,'addData']);
+
+
+      //Route::put('adminDashbord/update',[adminDBcontroller::class,'update']);
+        // Route::get('/test', function () {
+    //     return view('test', ['name' => "hi"]);
+    // });
+
+    //Route::put('test',[adminController::class,'update']);
+    //Route::get('admin',[sliderController::class,'showslider']);
+    
+    
+    
+    
+    
+    
+    
+    
+    //login route
+//     //Route::post("user",[UserAuth::class,'userLogin']);
+//     Route::view("/loginPage",'loginPage');
+//     Route::view("/islam",'islam');
+
+
+
+//     //sginup route
+//     Route::post('/loginPage',[customercontroler::class,'AddCustomer']);
+// //    Route::post('/loginPage','customercontroler@AddCustomer');
